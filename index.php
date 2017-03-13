7caee3b9324b135e33754d90e0d70fa330239c10
Katdelorme <Katdelorme@yahoo.com>
2017-03-13 12:14:58 -0400

    Outline the first four sections of the home page

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>App Landing Page | Coded by Katherine Delorme</title>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+
+<body>
+<header>
+  <nav>
+  </nav>
+</header>
+
+<section>
+  <div>
+    <h2>Madari is the Best</h2>
+    <h1>Best Landing Page to Present Your App</h1>
+    <a href=""><i class="fa fa-download fa-2x" aria-hidden="true"></i> Download</a>
+    <a href="">Learn More</a>
+  </div>
+  <div>
+    <img src="" alt="two phones displaying the app tilted to the left" />
+  </div>
+  <div>
+    <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i>
+    <p>25k Download</p>
+    <i class="fa fa-line-chart fa-2x" aria-hidden="true"></i>
+    <p>19k Active Accounts</p>
+    <i class="fa fa-star fa-2x" aria-hidden="true"></i>
+    <p>4.5 App Rating</p>
+  </div>
+</section>
+
+<section>
+  <h3>About Madari</h3>
+  <div>
+    <img src="" alt="hand holding iphone featuring the app" />
+  </div>
+  <div>
+    <h4><span>Madari</span>is the best app for your business</h4>
+    <p>Qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis itempor incididunt ut labore et dolore magna aliqua.</p>
+    <p>Ut enim ad minim vienia no<br/>strud exercitation ullamco laboris.nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate goi velit esse cillum dolore eu fugiat nulla pariatur.</p>
+    <a href=""><i class="fa fa-android fa-2x" aria-hidden="true"></i> Download</a>
+    <a href=""><i class="fa fa-apple fa-2x" aria-hidden="true"></i> Download</a>
+  </div>
+</section>
+
+<section>
+  <h3>App Features</h3>
+  <div>
+    <i class="fa fa-cogs fa-2x" aria-hidden="true"></i>
+    <i class="fa fa-gift fa-2x" aria-hidden="true"></i>
+    <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+    <h4>Great & Fast service</h4>
+    <p>Duis aute irure dolor ireprehenderit in voluptate velit esse cillum dolor. eu fugiat nulla pariatur.</p>
+    <h4>High quality products</h4>
+    <p>Duis aute irure dolor ireprehenderit in voluptate velit esse cillum dolor. eu fugiat nulla pariatur.</p>
+    <h4>Creative design</h4>
+    <p>Duis aute irure dolor ireprehenderit in voluptate velit esse cillum dolor. eu fugiat nulla pariatur.</p>
+  </div>
+  <div>
+    <img src="" alt="A page of the app displayed on an iphone" />
+  </div>
+  <div>
+    <i class="fa fa-cloud-upload fa-2x" aria-hidden="true"></i>
+    <i class="fa fa-gift fa-2x" aria-hidden="true"></i>
+    <i class="fa fa-mobile fa-2x" aria-hidden="true"></i>
+    <h4>Daily New Update</h4>
+    <p>Duis aute irure dolor ireprehenderit in voluptate velit esse cillum dolor. eu fugiat nulla pariatur.</p>
+    <h4>High quality products</h4>
+    <p>Duis aute irure dolor ireprehenderit in voluptate velit esse cillum dolor. eu fugiat nulla pariatur.</p>
+    <h4>Ultra responsive</h4>
+    <p>Duis aute irure dolor ireprehenderit in voluptate velit esse cillum dolor. eu fugiat nulla pariatur.</p>
+  </div>
+</section>
+
+<section>
+  <h3>Awesome Services</h3>
+  <div>
+    <i class="fa fa-calendar" aria-hidden="true"></i>
+    <h4>Events & Counters</h4>
+    <p>laboris nisi ut aliquip ex ea in commodo consequat. Duis aute nisi eprehenderit in voluptate velit es laboris ut.</p>
+  </div>
+  <div>
+    <i class="fa fa-cloud-download" aria-hidden="true"></i>
+    <h4>Data Storage</h4>
+    <p>laboris nisi ut aliquip ex ea in commodo consequat. Duis aute nisi eprehenderit in voluptate velit es laboris ut.</p>
+  </div>
+  <div>
+    <i class="fa fa-lock" aria-hidden="true"></i>
+    <h4>Security</h4>
+    <p>laboris nisi ut aliquip ex ea in commodo consequat. Duis aute nisi eprehenderit in voluptate velit es laboris ut.</p>
+  </div>
+</section>
+
+<section>
+  <h3>App Screenshot</h3>
+</section>
+
+<section>
+  Later
+</section>
+
+<section>
+</section>
+
+<footer>
+</footer>
+</body>
+</html>
